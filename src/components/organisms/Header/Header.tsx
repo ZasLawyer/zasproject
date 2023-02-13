9f66802c49758eed722eac2057ba1af2d4cc055f
Esteban Palomeque <shaggy.palomeque@gmail.com>
2023-02-12 21:37:19 -0500

    Create header organism

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,5 +1,9 @@
 import { NavBar } from '@/components/molecules/NavBar'
 
 export function Header() {
-    return <NavBar />
+    return (
+        <header className="relative w-full h-20">
+            <NavBar />
+        </header>
+    )
 }
